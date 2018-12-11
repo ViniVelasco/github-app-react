50f617d199696cab3c2056ad9b0d4e44b864cd70
= <sasukegc@gmail.com>
2018-12-11 20:39:43 -0200

    Initial commit

---
+++ src/components/repos.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+
+const Repos = ({ className, title, repos }) => (
+  <div className={className}>
+    <h2>{title}</h2>
+    <ul>
+      {repos.map((repo, index) => (
+        <li key={index}>
+          <a href={repo.link}>{repo.name}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+Repos.defaultProps = {
+  classname: '',
+  title: ''
+}
+
+Repos.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  respos: PropTypes.array
+}
+
+export default Repos
