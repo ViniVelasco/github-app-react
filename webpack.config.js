2513b655be29e186e494cc33c01a39dc4221f10a
= <sasukegc@gmail.com>
2018-12-15 15:57:47 -0200

    Adicionando assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path')
 const webpack = require('webpack')
 const validate = require('webpack-validator')
 
+const HtmlPlugin = require('html-webpack-plugin')
+
 module.exports = validate({
   devtool: 'source-map',
 
@@ -16,14 +18,18 @@ module.exports = validate({
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    filename: '[name]-[hash].js',
+    publicPath: ''
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlPlugin({
+      title: 'Github app',
+      template: path.join(__dirname, 'src', 'html', 'template.html')
+    })
   ],
-
+ 
   module: {
     preLoaders: [{
       test: /\.js$/,
@@ -37,6 +43,11 @@ module.exports = validate({
       exclude: /node_modules/,
       include: /src/,
       loader: 'babel'
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loaders: ['style', 'raw']
     }]
   }
 })
