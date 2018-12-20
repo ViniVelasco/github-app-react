d8688a53bb09e73e810e1f0cb8ce1d49dabcd049
= <sasukegc@gmail.com>
2018-12-20 21:35:30 -0200

    Criando paginação

---
+++ src/components/utils/index.js
@@ -0,0 +1,49 @@
+const centerRule = ({total, activePage}) => {
+  if(activePage - 1 <= 0) {
+    return 1
+  }
+  return activePage - 1
+}
+
+const pagination = ({ total, activePage }) => { //destructuring
+  // return Array.apply(null, { length: total }).map((_, i) => i + 1)
+  if(total <= 5 ) {
+    return Array.from({ length: total }, (_, i) => i + 1)
+  } 
+
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({ length: visiblePages }, (_, i) => i + centerRule({ total, activePage })),
+    total
+  ]
+
+  pages = pages.filter((page, index, array) => array.indexOf(page) === index)
+
+  let penultimatePage = pages[pages.length - 2]
+  let lastPage = pages[pages.length - 1]
+
+  if(penultimatePage === (lastPage - 2)) {
+    pages = [
+      ...pages.slice(0, -1),
+      lastPage - 1,
+      lastPage
+    ]
+  }
+
+  // Array can change
+  penultimatePage = pages[pages.length - 2]
+  lastPage = pages[pages.length - 1]
+
+  if(penultimatePage <= (lastPage - 2)){
+    pages = [
+      ...pages.slice(0, -1),
+      '...',
+      lastPage
+    ]
+  }
+
+  return pages
+}
+
+export default pagination
